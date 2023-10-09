70ba07cefc204896c212f056646c1aac102b1abe
Zachy9283 <zach.brown@student.csulb.edu>
2023-10-09 15:40:26 -0700

    Revert "Enhance UI changes"
    This reverts commit dc5d9935db4c7f254718ab32951ee85580ca6a89.

--- Assets/Scripts/All/Upgrade & Evolve/Upgrade Scripts/Upgrade Tab Controller.cs
+++ Assets/Scripts/All/Upgrade & Evolve/Upgrade Scripts/Upgrade Tab Controller.cs
@@ -5,27 +5,37 @@ using UnityEngine.UI;
 
 public class UpgradeTabController : MonoBehaviour
 {
-    public GameObject upgradeCanvas, buttonCanvas;
-    public GameObject SelectionCanvas;
     public GameObject upgradeTab, evolveTab;
+    public GameObject upgradeUnit, evolveUnit;
+    public GameObject upgradeMaterial, evolveMaterial;
+    public Button upgradeTabButton, evolveTabButton;
 
     void Awake()
     {
-        upgradeCanvas.SetActive(false);
+        OnUpgradeTab();
     }
+
     public void OnUpgradeTab()
     {
-        buttonCanvas.SetActive(false);
-        SelectionCanvas.SetActive(true);
+        upgradeUnit.SetActive(true);
+        evolveUnit.SetActive(false);
         upgradeTab.SetActive(true);
         evolveTab.SetActive(false);
+        upgradeMaterial.SetActive(true);
+        evolveMaterial.SetActive(false);
+        upgradeTabButton.interactable = false; 
+        evolveTabButton.interactable = true;
     }
 
     public void OnEvolveTab()
     {
-        buttonCanvas.SetActive(false);
-        SelectionCanvas.SetActive(true);
+        evolveUnit.SetActive(true);
+        upgradeUnit.SetActive(false);
         evolveTab.SetActive(true);
         upgradeTab.SetActive(false);
+        evolveMaterial.SetActive(true);
+        upgradeMaterial.SetActive(false);
+        evolveTabButton.interactable = false;
+        upgradeTabButton.interactable = true;
     }
 }
