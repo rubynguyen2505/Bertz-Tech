9750465c61618733155ea5b644c202f2c71c14c5
rubynguyen2505 <rubynguyen2505@gmail.com>
2023-11-07 11:30:35 -0800

    Added Player's Level and EXP Testing

---
+++ Assets/Scripts/All/UI/Home Screen/PlayerController.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private Slider experienceSlider;
+    [SerializeField] private TextMeshProUGUI levelText;
+
+    private int playerLevel;
+    private int currentEXP;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerLevel = 1;
+        levelText.text = playerLevel.ToString();
+        currentEXP = 0;
+        experienceSlider.value = currentEXP;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentEXP >= experienceSlider.maxValue)
+        {
+            playerLevel++;
+            levelText.text = playerLevel.ToString();
+            currentEXP = 0;
+            experienceSlider.maxValue = (int)(100 * (1.0f + playerLevel * 0.1f));
+            experienceSlider.value = currentEXP;
+        }
+    }
+
+    public void GainExperience()
+    {
+        currentEXP += 50;
+        experienceSlider.value = currentEXP;
+    }
+}
